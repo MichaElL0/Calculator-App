39b22e6a895ac4ebb72f86b6651ecf4b5e535dbd
MichaEIL0 <dkujoo@gmail.com>
2024-04-20 19:06:46 +0200

    Add operate function which makes logic of calculator

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let number1 = 0;
+let number2 = 0;
+let operator = "";
+
 function add(num1, num2) {
     return num1 + num2;
 }
@@ -12,6 +16,22 @@ function multiply(num1, num2) {
 
 function divide(num1, num2) {
     return num1 / num2;
+}
+
+function operate(_operator, _num1, _num2) {
+    switch(_operator) {
+        case "+":
+            return add(_num1, _num2);
+        case "-":
+            return subtract(_num1, _num2);
+        case "*":
+            return multiply(_num1, _num2);
+        case "/":
+            return divide(_num1, _num2);
+        default:
+            return "No such operator";
+
+    }
 }
 
 
