f238efc74f818293d647a6d4acdd4035056056f6
MichaEIL0 <dkujoo@gmail.com>
2024-04-25 18:55:14 +0200

    Add display logic and operator logic. Basic calculator logic done

--- script.js
+++ script.js
@@ -34,9 +34,57 @@ function operate(_operator, _num1, _num2) {
     }
 }
 
-let cos = "1.4";
+
+//Make digit buttons work and appear on the display
+const display = document.querySelector(".display");
+let displayValue = "";
+const buttons = document.querySelectorAll(".digit");
+
+function resetDisplay() {
+    display.textContent = "0";
+    displayValue = "";
+}
+
+buttons.forEach(item => {
+    item.addEventListener("click", event => {
+        if(displayValue.length >= 10) {
+            return;
+        }
+        else {
+            display.textContent = "";
+            displayValue += event.target.textContent;
+            display.textContent = displayValue;
+        }
+    });
+});
 
 
+//Make clear button work
+const clear = document.querySelector("#clear");
+clear.addEventListener("click", event => {
+    number1 = 0;
+    number2 = 0;
+    operator = "";
+    resetDisplay();
+});
 
 
+//Make operator buttons work
+const operators = document.querySelectorAll(".operator");
+operators.forEach(item => {
+    item.addEventListener("click", event => {
+        operator = event.target.textContent;
+        number1 = parseFloat(displayValue);
+        resetDisplay();
+        //alert(operator);
+    });
+});
 
+
+//Make equals button work
+const equals = document.querySelector("#equals");
+equals.addEventListener("click", event => {
+    number2 = parseFloat(displayValue);
+    resetDisplay();
+    display.textContent = operate(operator, number1, number2);
+});
