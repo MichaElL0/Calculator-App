73b3201d2dec34f4123cdb2ba2ebf671b88c2527
MichaEIL0 <dkujoo@gmail.com>
2024-05-09 20:12:10 +0200

    Fix calculator logic 2.0

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-let preNumber = 0;
-let curNumber = 0;
-let operator = "";
-let equalClicked = false;
-
 function add(num1, num2) {
     return num1 + num2;
 }
@@ -31,10 +26,8 @@ function operate(_operator, _num1, _num2) {
             return divide(_num1, _num2);
         default:
             return "No such operator";
-
     }
 }
-
 
 //Make digit buttons work and appear on the display
 const display = document.querySelector(".display");
@@ -45,43 +38,59 @@ const equals = document.querySelector("#equals");
 const calculator = document.querySelector(".calculator");
 const everyButton = document.querySelectorAll(".button");
 let displayValue = 0;
+let preNumber = 0;
+let curNumber = 0;
+let operator = "";
+let equalClicked = false;
+
+function reset() {
+    curNumber = 0;
+    preNumber = 0;
+    displayValue = "";
+    operator = "";
+    display.textContent = "0";
+}
 
 everyButton.forEach(item => {
     item.addEventListener("click", event => {
         if(event.target.classList.contains("digit")) {
             if(display.textContent.length <= 9) {
+                // display.textContent = parseFloat(curNumber);
+                // displayValue += event.target.textContent;
+                // curNumber = parseFloat(displayValue);
+                // display.textContent = parseFloat(displayValue);
+                curNumber += event.target.textContent;
                 display.textContent = parseFloat(curNumber);
-                //curNumber = 0;
-                displayValue += event.target.textContent;
-                display.textContent = parseFloat(displayValue);
-                curNumber = parseFloat(displayValue);
-                //curNumber += event.target.textContent;
             }
-            //curNumber = parseFloat(curNumber);
         }
         else if(event.target.classList.contains("operator")) {
-           
+            // operator = event.target.textContent;
+            // preNumber = parseFloat(displayValue);
+            // curNumber = 0;
+            // displayValue = 0;
+            operator = event.target.textContent;
+            preNumber = parseFloat(display.textContent);
+            curNumber = 0;
+
         }
         else if(event.target.classList.contains("equals")) {
-            if(display.textContent.length <= 9) {
-                displayValue = operate(operator, parseFloat(preNumber), parseFloat(curNumber));
-                display.textContent = Math.trunc(displayValue*100000000)/100000000;
-                //curNumber = displayValue;
-                preNumber = displayValue;
-                curNumber = 0;
+            if(curNumber == 0 || preNumber == 0 && operator == "/") {
+                alert("Don't break me... :(");
+                reset();
+                return;
             }
+            //displayValue = operate(operator, parseFloat(preNumber), parseFloat(curNumber));
+            //displayValue = Math.trunc(operate(operator, parseFloat(preNumber), parseFloat(curNumber))*10000000)/10000000;
+            display.textContent = Math.trunc(operate(operator, parseFloat(preNumber), parseFloat(curNumber))*10000000)/10000000;
+            preNumber = parseFloat(display.textContent);
+            curNumber = 0;
         }
         else if(event.target.classList.contains("clear")) {
-            curNumber = 0;
-            preNumber = 0;
-            displayValue = "";
-            operator = "";
-            display.textContent = "0";
+            reset();
         }
-        console.log(`Current number: ${curNumber}, previous number: ${preNumber}, display value: ${displayValue}`);
+        console.log(`Current number: ${curNumber}, previous number: ${preNumber}, display value: ${displayValue} and operator: ${operator}`);
     });
 });
-
 
 
 //Make box shadow effect
